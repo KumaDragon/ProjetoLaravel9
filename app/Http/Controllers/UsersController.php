7da4b7c69acb4450ae2be903fbc9dc9f35b7e08d
Thales <thales.b.sousa@gmail.com>
2023-04-19 21:31:34 -0300

    seed

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -1,14 +1,16 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function index()
     {
-    return view('users.index');
+        $users = User::get();
+        dd($users);
+        //return view('users.index');
     }
 
     public function show($id)
